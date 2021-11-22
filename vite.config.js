0f71f70d83a8e797239f789a0f8358ce673e428f
orangefour <orange@four>
2021-11-22 16:18:40 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import svelte from '@svitejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '..',
+    emptyOutDir: false
+  },
   plugins: [svelte()],
 })
